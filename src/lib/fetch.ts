36d6ece1b7a2bebb1f2d7f626b01e729f66f668b
Edy Hwang <42156235+edhwang90@users.noreply.github.com>
2022-09-05 13:00:52 -0400

    ts
    fetch

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,19 +1,23 @@
 import exchangeRates from "./rates.json";
 
 // sleep helper to simulate a network call
-const sleep = (time) => new Promise((res) => setTimeout(res, time));
+const sleep = (time:number) => new Promise((res) => setTimeout(res, time));
 
 // since we only make a single fetch call
 // we're going to use this mock fetch method
 // which will simulate a short delay and return
 // with exchange rates after a short period
-export async function fetch(url) {
+export async function fetch(url:string) {
   const [, search = ""] = url.split("?");
   const params = new URLSearchParams(search);
   await sleep(400);
   const symbols = new Set((params.get("symbols") || "").split(","));
-  const base = params.get("base");
-  const rates = exchangeRates[base];
+  const base:string = params.get("base")!;
+
+  // Todo:
+  // Due to data structure at rates.json and being mock
+  const getExchangeRates:any = exchangeRates;
+  const rates:any = getExchangeRates[base];
   return {
     // simulate the JSON method on the fetch response
     async json() {
